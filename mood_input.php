2717ed86ec308ff09dc124bd77472cb1fea0815d
Hally149 <Osaserehallyero@gmail.com>
2026-06-09 00:25:29 +0100

    Initial commit

---
+++ mood_input.php
@@ -0,0 +1,11 @@
+# frontend
+
+<?php
+$mood = $_POST['mood'];
+file_put_contents("mood.txt", $mood);
+echo "Mood updated!";
+?>
+
+
+# Copyright © 2026 Osasere H. Ero. All rights reserved.
+# Proprietary and confidential. Unauthorized copying of this file, via any medium, is strictly prohibited.
