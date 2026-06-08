92f4e78321cdf7c83dc73b2f3f3c8b82459de5da
Hally149 <Osaserehallyero@gmail.com>
2026-06-09 00:16:02 +0100

    Initial commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('dashboard.html')
-
+ 
 @app.route('/api/emotion', methods=['POST'])
 def emotion_api():
     image = request.files['image']
